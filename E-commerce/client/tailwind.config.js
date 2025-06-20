b34b91151abe9a8b974ef35893f9ac60940facd1
Ashwin052 <ashwinsriharir@gmail.com>
2025-06-20 10:44:29 +0530

    Delete E-commerce directory

--- E-commerce/client/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-// tailwind.config.js
-module.exports = {
-  darkMode: "class", // or "media" for system preference
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
